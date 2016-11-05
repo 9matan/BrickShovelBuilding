bc5c7d86548d6ca984859e012376d47f1dfd5076
elsq <elsq@mail.ru>
2016-11-05 21:56:36 +0200

    asd

---
+++ Assets/Content/Level/UI/TextUpdate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using BSB;
+
+public class TextUpdate : MonoBehaviour
+{
+  public Text funds_value;
+  public Text materials_value;
+  public Text funds_value_profit;
+  public Text materials_value_profit;
+  public Text workers_value;
+  public Text workers_free;
+
+  void Update()
+  {
+    funds_value.text = BSBDirector.playerResources.funds.ToString();
+    materials_value.text = BSBDirector.playerResources.materials.ToString();
+    funds_value_profit.text = "+" + BSBDirector.playerResources.income.funds.ToString();
+    materials_value_profit.text = "+" + BSBDirector.playerResources.income.materials.ToString();
+    workers_value.text = "/" + BSBDirector.playerResources.workersCapacity.ToString();
+    workers_free.text = BSBDirector.playerResources.workers.ToString();
+  }
+}
